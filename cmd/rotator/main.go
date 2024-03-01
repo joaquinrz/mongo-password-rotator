c5c89d4e6c8dea62c3513c6adb8ad6759ecc3a19
Joaquin Rodriguez <rjoaquin@microsoft.com>
2024-02-29 18:12:25 -0600

    initial release

--- cmd/rotator/main.go
+++ cmd/rotator/main.go
@@ -4,68 +4,70 @@ import (
 	"context"
 	"log"
 	"path/filepath"
+	"sync"
 	"time"
+
+	"github.com/joaquinrz/mongo-password-rotator/internal/mongodb"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/joaquinrz/mongo-password-rotator/internal/config"
-	"github.com/joaquinrz/mongo-password-rotator/internal/mongodb"
 )
 
 func main() {
-	log.Println("Starting MongoDB password rotator.")
+	log.Println("MongoDB password rotator started.")
 
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
+	if err := watchPasswordFileChanges(cfg); err != nil {
+		log.Fatalf("Error watching password file: %v", err)
+	}
+}
+
+func watchPasswordFileChanges(cfg *config.Config) error {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatalf("Failed to create file watcher: %v", err)
+		return err
 	}
 	defer watcher.Close()
 
-	err = watcher.Add(filepath.Dir(cfg.NewPasswordFilePath))
-	if err != nil {
-		log.Fatalf("Failed to watch the directory for password file changes: %v", err)
+	dirPath := filepath.Dir(cfg.NewPasswordFilePath)
+	if err := watcher.Add(dirPath); err != nil {
+		return err
 	}
 
-	log.Println("Monitoring for password changes...")
+	var (
+		cooldownPeriod = 2 * time.Second
+		lastUpdate     time.Time
+		mu             sync.Mutex
+	)
 
-	listenForEvents(watcher, cfg)
-
-	// Block main goroutine indefinitely.
-	select {}
-}
-
-func listenForEvents(watcher *fsnotify.Watcher, cfg *config.Config) {
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return // Exit if the channel is closed.
-				}
-				handleEvent(event, cfg)
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return // Exit if the channel is closed.
-				}
-				log.Printf("Watcher error: %v", err)
+	log.Println("Waiting for Password file changes...")
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return nil // Exit if channel is closed.
 			}
-		}
-	}()
-}
 
-func handleEvent(event fsnotify.Event, cfg *config.Config) {
-	// Check if the event is related to the NewPasswordFilePath.
-	if filepath.Clean(event.Name) == filepath.Clean(cfg.NewPasswordFilePath) {
-		switch {
-		case event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create:
-			log.Printf("Detected change in password file: %s", event.Name)
-			updateMongoDBPassword(cfg)
-		case event.Op&fsnotify.Remove == fsnotify.Remove:
-			log.Printf("Password file was deleted: %s", event.Name)
+			if filepath.Base(event.Name) == filepath.Base(cfg.NewPasswordFilePath) && time.Since(lastUpdate) > cooldownPeriod {
+				mu.Lock()
+				if time.Since(lastUpdate) > cooldownPeriod {
+					log.Println("Password file changed, updating MongoDB password...")
+					updateMongoDBPassword(cfg)
+					lastUpdate = time.Now()
+					log.Println("Waiting for Password file changes...")
+				}
+				mu.Unlock()
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return nil // Exit if channel is closed.
+			}
+			log.Printf("Watcher error: %v", err)
 		}
 	}
 }
